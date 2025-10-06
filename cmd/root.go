a0d15e36a26e145c00f95115458451bc1b91358a
Thomas GREGOIRE <thomas.gregoire82@gmail.com>
2025-10-06 21:39:22 +0200

    Add custom Help flag

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"ClickOnMe/checker"
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -22,7 +23,17 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+
+	rootCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+
+		fmt.Printf("%s\n\n\n", rootCmd.Long)
+		fmt.Printf("Usage :\n\t./ClickOnMe <[-i PathToInputFile] or [-u URL]> [-o PathToOutputFile] [-r nbRequestPerSecond]\n\n")
+		fmt.Printf("Flag :\n\t-h, --help			Print Help\n\n")
+		fmt.Printf("Options :\n\t-i, --inputFile		Path to the file containing URLs\n\t-o, --outputFile	Path to the file that will contain results\n\t-u, --URL			URL that must be checked\n\t-r, --rate			Number of request per second (default: 25 r/s)\n\n")
+		os.Exit(0)
+	})
 	err := rootCmd.Execute()
+
 	checker.StartChecking()
 	if err != nil {
 		os.Exit(1)
@@ -32,7 +43,7 @@ func Execute() {
 func init() {
 	rootCmd.Flags().BoolP("help", "h", false, "Help message for toggle")
 	rootCmd.Flags().StringVarP(&checker.FileURLs, "inputFile", "i", "", "Input File containing URLs")
-	rootCmd.Flags().StringVarP(&checker.OutputFile, "outputFile", "o", "", "Output File containing results")
+	rootCmd.Flags().StringVarP(&checker.OutputFile, "outputFile", "o", "", "Output File that will contain results")
 	rootCmd.Flags().StringVarP(&checker.URL, "url", "u", "", "URL that must be checked")
 	rootCmd.Flags().IntVarP(&checker.RateLimit, "rate", "r", 25, "Rate limit for the number of request that must be send every second")
 }
